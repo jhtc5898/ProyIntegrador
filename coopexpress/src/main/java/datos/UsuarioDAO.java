455bc29fa277b0f14eaf82f3762f5faf1cea41e8
danie <danie@192.168.1.4>
2019-12-18 16:22:00 -0500

    Creacion de la clase Usuario

--- coopexpress/src/main/java/datos/UsuarioDAO.java
+++ coopexpress/src/main/java/datos/UsuarioDAO.java
@@ -1,5 +1,34 @@
 package datos;
 
-public class UsuarioDAO {
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class UsuarioDAO {
+	
+@Id
+@Column(name="cedula_usuario")
+private String cedula;
+
+@Column(name="nombre_usuario")
+private String nombre;
+
+@Column(name="apellido_usuario")
+private String apellido;
+
+@Column(name="telefono_usuario")
+private String telefono;
+
+@Column(name="direccion_usuario")
+private String direccion;
+
+@Column(name="usuario_correo")
+private String correo;
+
+	
 }
