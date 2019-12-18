78ef3a01603464130576b9b628cbdf52b6125c20
danie <danie@192.168.1.4>
2019-12-18 16:28:12 -0500

    Creacion de la clase Usuario Dao

--- coopexpress/src/main/java/datos/UsuarioDAO.java
+++ coopexpress/src/main/java/datos/UsuarioDAO.java
@@ -2,33 +2,49 @@ package datos;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
-@Entity
+import modelo.Usuario;
+
+@Stateless
 public class UsuarioDAO {
 	
-@Id
-@Column(name="cedula_usuario")
-private String cedula;
-
-@Column(name="nombre_usuario")
-private String nombre;
-
-@Column(name="apellido_usuario")
-private String apellido;
-
-@Column(name="telefono_usuario")
-private String telefono;
-
-@Column(name="direccion_usuario")
-private String direccion;
-
-@Column(name="usuario_correo")
-private String correo;
-
+	@Inject
+	private EntityManager em;
 	
+	public void insert(Usuario usuario) {
+		em.persist(usuario);
+	}
+	
+	public void update(Usuario usuario) {
+		em.merge(usuario);
+	}
+	
+	public void remove(String cedula) {
+		em.remove(this.read(cedula));
+	}
+	
+	public Usuario read(String cedula) {
+		return em.find(Usuario.class, cedula);
+	}
+	
+	public List<Usuario> getUsuario(){
+		String jpql = "SELECT u FROM Usuario u";
+		Query q = em.createQuery(jpql, Usuario.class);
+		
+		List<Usuario> usuarios = q.getResultList();
+		
+		return usuarios;
+	}
+	
+	public List<Usuario> getUsuarioCedula(String cedula){
+		String jpql = "SELECT u FROM Usuario u WHERE cedula LIKE ?1";
+		Query q = em.createQuery(jpql, Usuario.class);
+		q.setParameter(1, "%" + cedula + "%");
+		List<Usuario> usuarios= q.getResultList();
+		return usuarios;
+	}
 }
--- coopexpress/src/main/java/modelo/Usuario.java
+++ coopexpress/src/main/java/modelo/Usuario.java
@@ -1,12 +1,8 @@
 package modelo;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Usuario {
