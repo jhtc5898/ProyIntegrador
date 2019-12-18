4fc78f397fb751a49b9f85277b296cf492235696
danie <danie@192.168.1.4>
2019-12-18 16:37:06 -0500

    Clase gestionusuarios creada

--- coopexpress/src/main/java/negocio/GestionUsuarios.java
+++ coopexpress/src/main/java/negocio/GestionUsuarios.java
@@ -1,5 +1,38 @@
 package negocio;
 
-public class GestionUsuarios {
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import datos.UsuarioDAO;
+import modelo.Usuario;
+
+@Stateless
+public class GestionUsuarios {
+	
+	@Inject
+	private UsuarioDAO usuarioDAO;
+	
+	public void guardarUsuario(Usuario usuario) {
+		if(usuarioDAO.read(usuario.getCedula())==null) {
+			usuarioDAO.insert(usuario);
+		}else {
+			usuarioDAO.update(usuario);
+		}
+	}
+	
+	public void eliminarUsuario(String cedula) {
+		if(usuarioDAO.read(cedula)!=null) {
+			usuarioDAO.remove(cedula);
+		}
+	}
+	
+	public void actualizarUsuario(Usuario usuario) {
+			usuarioDAO.update(usuario);
+	}
+	
+	public List<Usuario> getUsuarioCedula(String cedula) {	
+		return usuarioDAO.getUsuarioCedula(cedula);
+	}
 }
