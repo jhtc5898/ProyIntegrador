8ee33d03b4984f0a843e3bd14387e8d565d2d2ee
javier <javier@192.168.1.8>
2019-12-18 16:00:36 -0600

    creacion metodos DAO

--- coopexpress/src/main/java/datos/CuentaDAO.java
+++ coopexpress/src/main/java/datos/CuentaDAO.java
@@ -1,5 +1,53 @@
 package datos;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import modelo.Cuenta;
+
+@Stateless
 public class CuentaDAO {
 
+	@Inject
+	private EntityManager em;
+	
+	public void insert(Cuenta cuenta) {
+		em.persist(cuenta);
+	}
+	
+	public void update(Cuenta cuenta) {
+		em.merge(cuenta);
+		
+	}
+	
+	public void remove(int codigo) {
+		Cuenta cuenta  = this.read(codigo);
+		em.remove(cuenta);
+	}
+	public Cuenta read(int codigo) {
+		Cuenta c = em.find(Cuenta.class, codigo);
+		return c;
+	}
+	
+	public List<Cuenta> getCuentas(){
+		String jpql = "SELECT c FROM cuenta";
+		Query q = em.createQuery(jpql,Cuenta.class);
+		
+		List<Cuenta> cuentas = q.getResultList();
+		return cuentas;
+	}
+	
+	public Cuenta getCuentaNombre(int numeroCuenta){
+		String jpql = "SELECT c FROM Cuenta c WHERE numero_cuenta LIKE ?1";
+		Query q = em.createQuery(jpql,Cuenta.class);
+		
+		Cuenta c = (Cuenta) q.getSingleResult();
+		
+		return c;
+	}
+	
 }
--- coopexpress/src/main/java/modelo/Cuenta.java
+++ coopexpress/src/main/java/modelo/Cuenta.java
@@ -1,5 +1,56 @@
 package modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cuenta {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "codigo_cuenta")
+	private int codigo_cuenta;
+	
+	@Column(name = "numero_cuenta")
+	private String numero_cuenta;
+	
+	@Column(name = "saldo_cuenta")
+	private double saldo_cuenta;
+
+	public int getCodigo_cuenta() {
+		return codigo_cuenta;
+	}
+
+	public void setCodigo_cuenta(int codigo_cuenta) {
+		this.codigo_cuenta = codigo_cuenta;
+	}
+
+	public String getNumero_cuenta() {
+		return numero_cuenta;
+	}
+
+	public void setNumero_cuenta(String numero_cuenta) {
+		this.numero_cuenta = numero_cuenta;
+	}
+
+	public double getSaldo_cuenta() {
+		return saldo_cuenta;
+	}
+
+	public void setSaldo_cuenta(double saldo_cuenta) {
+		this.saldo_cuenta = saldo_cuenta;
+	}
+
+	@Override
+	public String toString() {
+		return "Cuenta [codigo_cuenta=" + codigo_cuenta + ", numero_cuenta=" + numero_cuenta + ", saldo_cuenta="
+				+ saldo_cuenta + "]";
+	}
+	
+	
+	
 
 }
